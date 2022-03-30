d4c3a2fe479aa0749feac394d05aee14c6f2cab7
lakshya-agarwal <43514654+lakshya-agarwal@users.noreply.github.com>
2022-03-30 23:11:43 +0530

    first

---
+++ src/main/java/org/test2/test2/App.java
@@ -0,0 +1,15 @@
+package org.test2.test2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        greeter g = new greeter();
+        System.out.println(g.hello());
+    }
+}
---
+++ src/main/java/org/test2/test2/greeter.java
@@ -0,0 +1,10 @@
+package org.test2.test2;
+
+public class greeter {
+
+	public String hello() {
+		
+		String h = "hi";
+		return h;
+	}
+}
